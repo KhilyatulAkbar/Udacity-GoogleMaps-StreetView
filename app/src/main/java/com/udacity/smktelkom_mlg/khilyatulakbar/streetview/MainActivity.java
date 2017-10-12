d0dd5ce13d88703a867e2925b05170ec69d5810b
Khilyatul Akbar <khilyaalakbar@gmail.com>
2017-10-12 12:42:19 +0700

    Pulau Merah in Street View

---
+++ app/src/main/java/com/udacity/smktelkom_mlg/khilyatulakbar/streetview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.udacity.smktelkom_mlg.khilyatulakbar.streetview;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+
+public class MainActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        // Set the panorama location on startup, when no
+        // panoramas have been loaded.
+
+        panorama.setPosition(new LatLng(-8.603850, 114.029903));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera, 10000);
+
+    }
+
+}
+
